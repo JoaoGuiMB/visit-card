f8576edc867dd144c188953258a6ca059e9d3461
JoaoGuiMB <juaoguimb159@gmail.com>
2024-02-04 13:55:42 -0300

    fix: suspense

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,19 +1,20 @@
 "use client";
-import { useSearchParams } from "next/navigation";
+
 import LandingImage from "@/components/Layout/LandingImage";
 import { useMedia } from "react-use";
 import VisitCard from "@/components/ResultPage/VisitCard";
-import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 const ResultPage = () => {
-  const searchParams = useSearchParams();
   const isMobile = useMedia("(max-width: 768px)");
-  console.log(searchParams.getAll("name"));
+
   return (
     <main className="w-full pt-36 h-[calc(100vh-72px)] bg-lightPurple flex-column justify-center items-center">
       <section className="flex flex-col grow justify-center items-center mt-14 md:my-24 md:flex-row md:justify-between md:mx-24">
         {!isMobile && <LandingImage />}
-        <VisitCard />
+        <Suspense fallback={<div>Loading...</div>}>
+          <VisitCard />
+        </Suspense>
       </section>
     </main>
   );
